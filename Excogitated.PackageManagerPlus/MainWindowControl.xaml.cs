2b076b0c612b4835fd084c2480a2c33c2b069d03
keith eskildsen <keithj17@gmail.com>
2020-06-28 20:40:20 -0500

    save before trying to convert project file

---
+++ Excogitated.PackageManagerPlus/MainWindowControl.xaml.cs
@@ -0,0 +1,33 @@
+﻿namespace Excogitated.PackageManagerPlus
+{
+    using System;
+    using System.Windows;
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for MainWindowControl.
+    /// </summary>
+    public partial class MainWindowControl : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindowControl"/> class.
+        /// </summary>
+        public MainWindowControl()
+        {
+            InitializeComponent();
+            _searchButton.KeyUp += _searchButton_KeyUp;
+        }
+
+        private async void _searchButton_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
